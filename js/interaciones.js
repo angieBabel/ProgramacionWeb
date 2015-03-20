a97a1fa08279d18079ccbc2d762eac00951f84d8
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-03-20 12:19:46 -0600

    update

--- js/interaciones.js
+++ js/interaciones.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('#accion').click(function(){
+	$('#add').click(function(){
 		var ncolumnas=$('tr:last td', $("#tabla")).length;
 		var nrenglon=$('#tabla tr').length;
 		var columna='<tr>';
@@ -18,10 +18,33 @@ $(document).ready(function(){
 		$("#tabla").append(columna);
 	});
 
-	$('#tabla tr').click(function{
-		background-color('yellow')
+	$('#del').click(function(){
+		var colum=$("#tabla tr").length;
+            if(colum>1)
+            {
+                $("#tabla tr:last").remove();
+            }
+
 	});
 
+	$('table tbody').on('click','tr', function(){
+		var nf=$(this).text();
+		var bc=$(this).css('background-Color');
+		alert(bc)
+		//0,128,0
+		/*if( ($(this).css('background-color'))== rgba(0,0,0,0) ){*/
+			//alert('aqui');
+			$(this).css('background-color','green');
+			$(this).css('color','yellow');			
+		/*}
+		else
+		{
+			
+			$(this).css('color','black');				
+		}*/
+
+		$('#nof').val(nf);
+	});
 });
 
 //poner de color y que fila es y si se da click de nuevo debe quitar el color y desaparecer
