2654f64125223a0bc015e6ae33027b4517adfbc0
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-03-20 10:49:16 -0600

    Agregar filas y contador

--- js/interaciones.js
+++ js/interaciones.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
 
-	alert('Se cargo la pagina');
 	$('#accion').click(function(){
-		$('#uno').toggle();
-	})
-	$('#uno').mouseenter(function(){
-		$(this).css('background-color','yellow');
-	})
+		var ncolumnas=$('tr:last td', $("#tabla")).length;
+		var nrenglon=$('#tabla tr').length;
+		var columna='<tr>';
+
+		for(var i=0;i<ncolumnas;i++){
+			
+			if (i>0){
+				columna+='<td></td>';
+			}else
+			{
+				columna+='<td>'+nrenglon+'</td>';
+			};
+		};
+		columna+='</tr>';
+		$("#tabla").append(columna);
+	});
+
+	$('#tabla tr').click(function{
+		background-color('yellow')
+	});
 
 });
-/*var a= function(){
-	console.log('texto');
-};
-function na(){
-	console.log('texto2');
-};*/
+
+//poner de color y que fila es y si se da click de nuevo debe quitar el color y desaparecer
