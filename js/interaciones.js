dc3a105642e32276977a677353f9f849aee10bf5
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-03-23 09:37:06 -0600

    Practicas
    Todas las practicas

---
+++ js/interaciones.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+	$('#add').click(function(){
+		var ncolumnas=$('tr:last td', $("#tabla")).length;
+		var nrenglon=$('#tabla tr').length;
+		var columna='<tr>';
+
+		for(var i=0;i<ncolumnas;i++){
+
+			if (i>0){
+				columna+='<td></td>';
+			}else
+			{
+				columna+='<td>'+nrenglon+'</td>';
+			};
+		};
+		columna+='</tr>';
+		$("#tabla").append(columna);
+	});
+
+	$('#del').click(function(){
+		var colum=$("#tabla tr").length;
+            if(colum>1)
+            {
+                $("#tabla tr:last").remove();
+            }
+
+	});
+
+//Cambia Color
+	$('table tbody').on('click','tr', function(){
+		var nf=$(this).text();
+		var bc=$(this).css('background-color');
+
+		if (bc === 'rgb(112, 186, 100)'){
+			$(this).css ('background-color','rgb(255, 255, 255)');
+			$(this).css('color','black');
+			$('#f').text('');
+
+		}else{
+			$(this).css('background-color','rgb(112, 186, 100)');
+			$(this).css('color','yellow');
+			$('#f').text(nf);
+			//$('#f').append(nf);
+		}
+
+	});
+
+
+	//Decir que fila es
+	$('table tbody').on('click','tr',function(){
+			var nf=$(this).text();
+			$('#nof').val(nf);
+
+	});
+
+});
+
+//poner de color y que fila es y si se da click de nuevo debe quitar el color y desaparecer
