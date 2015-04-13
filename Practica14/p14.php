978f666fd71c06abdad644601c20fd74ea7cc222
Angelica Barreda B <anjelyk.b@gmail.com>
2015-04-13 10:08:55 -0500

    Update

---
+++ Practica14/p14.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title> </title>
+  <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
+  <script type="text/javascript" src="external/jquery/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.js"></script>
+  <script>
+    function ajax1(){
+      $.ajax({
+          url:'ejercicio14b.php',
+          type:'POST',
+          data:'nom='+ $('#nom').val(),
+          success:function(data){
+            //Se ejecuta si no tiene errores
+            $('#contenido').html(data);
+          },
+          error:function(xhr,status){
+            //Se ejecuta si tuvo errores
+          },
+          complete:function(xhr,status){
+            //Se ejecut siempre
+            alert('Peticion terminada');
+          }
+      })
+    }
+  </script>
+</head>
+<body>
+  <input type="text" name="nom" id="nom" placeholder="Nombre">
+  <button id="bt1" onclick="ajax1()">Saludo</button>
+  <button id="bt2" onclick="$.ajax({})">Confirmar datos</button>
+  <div id="contenido"></div>
+</body>
+</html>
